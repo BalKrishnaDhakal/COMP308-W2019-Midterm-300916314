2ce5a4f32857d352cd15971babd08ac16d1e38be
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-25 00:34:07 -0500

    updated auth... functionality to view book list only by logined users

--- server/app.js
+++ server/app.js
@@ -1,95 +1,102 @@
-// moddules for node and express
-let createError = require('http-errors');
-let express = require('express');
-let path = require('path');
-let cookieParser = require('cookie-parser');
-let logger = require('morgan');
+    /* *********************************
+            Student Name: Bal Krishna Dhakal
+            Student ID: 300916314
+            Description: COMP308-W2019 Midterm Test
+            Date Created: 23rd Feb, 2019 
+            ************************************/
 
-// modules for authentication
-let session = require('express-session');
-let passport = require('passport');
-let passportLocal = require('passport-local');
-let localStrategy = passportLocal.Strategy;
-let flash = require('connect-flash');
+    // moddules for node and express
+    let createError = require('http-errors');
+    let express = require('express');
+    let path = require('path');
+    let cookieParser = require('cookie-parser');
+    let logger = require('morgan');
 
-
-// import "mongoose" - required for DB Access
-let mongoose = require('mongoose');
-// URI
-let config = require('./config/db');
-
-mongoose.connect(process.env.URI || config.URI, { useNewUrlParser: true });
-
-let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', () => {
-    console.log("Connected to MongoDB...");
-});
+    // modules for authentication
+    let session = require('express-session');
+    let passport = require('passport');
+    let passportLocal = require('passport-local');
+    let localStrategy = passportLocal.Strategy;
+    let flash = require('connect-flash');
 
 
-// define routers
-let index = require('./routes/index'); // top level routes
-let books = require('./routes/books'); // routes for books
+    // import "mongoose" - required for DB Access
+    let mongoose = require('mongoose');
+    // URI
+    let config = require('./config/db');
 
-let app = express();
+    mongoose.connect(process.env.URI || config.URI, { useNewUrlParser: true });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /client
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client')));
-
-//setup express-session
-app.use(session({
-    secret: "someSecrate",
-    saveUninitialized: false,
-    resave: false
-
-}));
-
-//initialize flash
-app.use(flash());
-
-//initializing passport
-app.use(passport.initialize());
-app.use(passport.session());
-
-// create User model
-let userModel = require('./models/user');
-let User = userModel.User;
-
-//implement a user authentication strategy
-passport.use(User.createStrategy());
-
-// serialize and deserialize the user info
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+    let mongoDB = mongoose.connection;
+    mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+    mongoDB.once('open', () => {
+        console.log("Connected to MongoDB...");
+    });
 
 
-// route redirects
-app.use('/', index);
-app.use('/books', books);
+    // define routers
+    let index = require('./routes/index'); // top level routes
+    let books = require('./routes/books'); // routes for books
+
+    let app = express();
+
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
+
+    // uncomment after placing your favicon in /client
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, '../client')));
+
+    //setup express-session
+    app.use(session({
+        secret: "someSecrate",
+        saveUninitialized: false,
+        resave: false
+
+    }));
+
+    //initialize flash
+    app.use(flash());
+
+    //initializing passport
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // create User model
+    let userModel = require('./models/user');
+    let User = userModel.User;
+
+    //implement a user authentication strategy
+    passport.use(User.createStrategy());
+
+    // serialize and deserialize the user info
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
+    // route redirects
+    app.use('/', index);
+    app.use('/books', books);
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        next(createError(404));
+    });
 
-module.exports = app;
+    // error handler
+    app.use(function(err, req, res, next) {
+        // set locals, only providing error in development
+        res.locals.message = err.message;
+        res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+        // render the error page
+        res.status(err.status || 500);
+        res.render('error');
+    });
+
+    module.exports = app;
