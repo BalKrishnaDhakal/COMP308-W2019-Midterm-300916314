711d9cd488809be0bb2e7c3c9c5ba22252cf25b5
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-24 20:05:15 -0500

    updated app.js to implement user model

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+// modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal = require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
+
 // import "mongoose" - required for DB Access
 let mongoose = require('mongoose');
 // URI
@@ -14,8 +22,8 @@ mongoose.connect(process.env.URI || config.URI, { useNewUrlParser: true });
 
 let mongoDB = mongoose.connection;
 mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
-mongoDB.once('open', ()=> {
-  console.log("Connected to MongoDB...");
+mongoDB.once('open', () => {
+    console.log("Connected to MongoDB...");
 });
 
 
@@ -36,6 +44,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
+//setup express-session
+app.use(session({
+    secret: "someSecrate",
+    saveUninitialized: false,
+    resave: false
+
+}));
+
+//initialize flash
+app.use(flash());
+
+//initializing passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // route redirects
 app.use('/', index);
@@ -44,18 +66,18 @@ app.use('/books', books);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
